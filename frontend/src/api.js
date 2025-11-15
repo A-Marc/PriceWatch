b2e0d2be40ef15d15ae7df70edcf5b7d987e50a8
GamySAF <gammiigamy@gmail.com>
2025-11-15 10:00:32 +0300

    commting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000" ||  "https://pricewatch-4n3q.onrender.com", // backend URL
+  baseURL:   "https://pricewatch-4n3q.onrender.com", // backend URL
 });
 
 export default API;
