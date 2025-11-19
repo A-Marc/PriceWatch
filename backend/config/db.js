857b06fa69e34c5739b2e17f3565923a215cab32
GamySAF <gammiigamy@gmail.com>
2025-11-19 15:57:39 +0300

    commitingg

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("MongoDB connection error occured:", err);
     process.exit(1); // exit process if DB connection fails
   }
 };
